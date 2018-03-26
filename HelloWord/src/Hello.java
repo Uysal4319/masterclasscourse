49b1c9e2a9f4194440d3e0756ae0dc8955975417
Ertuğrul UYSAL <37781800+Komur4319@users.noreply.github.com>
2018-03-26 16:14:45 +0300

    HelloWorld

---
+++ HelloWord/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+        int myFirstNumber=(10+5)+(2*10);
+        int mySecondNumber=12;
+        int myTheradNumber=myFirstNumber*2;
+
+        int totalNumber= myFirstNumber+mySecondNumber+myTheradNumber;
+        int myLastOne=1000-totalNumber;
+
+        System.out.println(myLastOne);
+
+    }
+}
