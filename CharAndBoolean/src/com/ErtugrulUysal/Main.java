b1c1601e4089970258c7793428372125e6b9dd4e
Ertuğrul UYSAL <37781800+Komur4319@users.noreply.github.com>
2018-03-27 14:42:07 +0300

    Char and Boolean Data Types

---
+++ CharAndBoolean/src/com/ErtugrulUysal/Main.java
@@ -0,0 +1,17 @@
+package com.ErtugrulUysal;
+
+public class Main {
+
+    public static void main(String[] args) {
+     //width of 16 = (2 byte).
+	char myChar='\u00A9';
+        System.out.println("Copyright : "+myChar);
+
+        boolean myBoolean=true;
+        boolean isMale=true;
+
+    char registered = '\u00AE';
+        System.out.println("Registered Sign : "+ registered);
+
+    }
+}
