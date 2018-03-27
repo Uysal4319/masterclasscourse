ac5b509c74730c629173943aa74a94ac2d8123f3
Ertuğrul UYSAL <37781800+Komur4319@users.noreply.github.com>
2018-03-27 14:35:41 +0300

    Float and Double Data Types

---
+++ FloatAndDouble/src/com/ErtugrulUysal/Main.java
@@ -0,0 +1,27 @@
+package com.ErtugrulUysal;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //width of int=32(4 Byte)
+    int myIntValue=5/3;
+    //width of float=32(4Byte)
+	float myFloatValue=5f /3f ;
+	//width of flooat=64(8Byte)
+	double myDoubleValue=5d / 3d;
+        System.out.println("MyIntValue= "+myIntValue);
+        System.out.println("MyFloatValue="+myFloatValue);
+        System.out.println("MyDoubleValue="+myDoubleValue);
+
+
+        float pound = 200f;
+        float kilogram=pound*0.45359237f;
+        System.out.println("Kilogram : "+kilogram);
+
+        double pi=3.1_415_927d;
+        System.out.println("pi : "+pi);
+        }
+    }
+
+
+
