e9ca516f343badee98a3034c705522bc2ed5cf50
Ertuğrul UYSAL <37781800+Komur4319@users.noreply.github.com>
2018-04-01 19:01:07 +0300

    If keyword in java

---
+++ Java Tutorial/IfKeywordAndCodeBlocks/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your Final Score : " + finalScore);
+
+        }
+        System.out.println("Second Score: " + score);
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your Final Score : " + finalScore);
+
+        }
+
+
+    }
+}
